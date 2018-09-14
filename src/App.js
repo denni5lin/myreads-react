2efd77dd3ae72f59521497e84111afd7801a8bcf
Dennis Lin <lin.dennis18@gmail.com>
2018-09-13 21:39:39 -0700

    Allows users to move books between shelves (bugs)

--- src/App.js
+++ src/App.js
@@ -8,16 +8,27 @@ class BooksApp extends React.Component {
   state = {
     books: []
   }
+
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({ books: books })
     })
   }
+
+  moveShelf = (book, shelf) => {
+    BooksAPI.update(book, shelf);
+
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books: books })
+    })
+  }
+
   render() {
     return (
       <div className="app">
         <MainPage 
           books={this.state.books}
+          moveShelf={this.moveShelf}
         />
       </div>
     )
