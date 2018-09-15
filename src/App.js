de9cdafb932aa60093174c8a50ccbd9a64e2bd8b
Dennis Lin <lin.dennis18@gmail.com>
2018-09-15 08:02:07 -0700

    Bug fix: Moving books to a different shelf works correctly

--- src/App.js
+++ src/App.js
@@ -9,18 +9,20 @@ class BooksApp extends React.Component {
     books: []
   }
 
+
+
   componentDidMount() {
     BooksAPI.getAll().then((books) => {
       this.setState({ books: books })
-    })
+    }) 
   }
 
   moveShelf = (book, shelf) => {
     BooksAPI.update(book, shelf);
-
+    
     BooksAPI.getAll().then((books) => {
       this.setState({ books: books })
-    })
+    }) 
   }
 
   render() {
@@ -29,7 +31,8 @@ class BooksApp extends React.Component {
         <MainPage 
           books={this.state.books}
           moveShelf={this.moveShelf}
-        />
+        />  
+
       </div>
     )
   }
